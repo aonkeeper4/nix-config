5c17e6c263519f02534fd373b4da56352c3a3a85
aonkeeper4 <aonkeeper4@gmail.com>
2026-09-07 11:52:20 +0100

    vscode is evil

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -1,8 +1,17 @@
-{ ... }: {
+{ pkgs, ... }:
+let
+  mullvad-autostart = pkgs.makeAutostartItem {
+    name = "mullvad-vpn";
+    package = pkgs.mullvad-vpn;
+  };
+in
+{
   # networking
   networking.hostName = "laptop-nixos";
   networking.networkmanager.enable = true;
 
   # vpn
-  services.mullvad-vpn.enable = true;
+  environment.systemPackages = [
+    mullvad-autostart
+  ];
 }
