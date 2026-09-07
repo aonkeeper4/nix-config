b86de6d52a8ce30ebb307fe96c83bfede415054a
aonkeeper4 <aonkeeper4@gmail.com>
2026-09-07 22:01:08 +0100

    ndsklfn

--- home/rider/default.nix
+++ home/rider/default.nix
@@ -1,11 +1,10 @@
-{ pkgs, ... }: {
+{ inputs, pkgs, ... }: {
   # rider
   home.packages =
     with pkgs;
-    with nix-jetbrains-plugins.lib;
+    with inputs.nix-jetbrains-plugins.lib;
     [
       (buildIdeWithPlugins pkgs "rider" [
-        "LJYXP.ColorBrackets"
         "color.scheme.Tokyo Night"
         "com.mallowigi.colorHighlighter"
       ])
