1935f4d94dd33b4fa4898186de1620093767d635
aonkeeper4 <aonkeeper4@gmail.com>
2026-09-07 12:24:06 +0100

    wawawa

--- home/vesktop/default.nix
+++ home/vesktop/default.nix
@@ -3,8 +3,8 @@
   home.packages = with pkgs; [
     vesktop
   ];
-  home.file.".config/vesktop/themes/midnight-tokyo-night-renogare.theme.css" =
+  home.file.".config/vesktop/themes/midnight-tokyo-night-renogare.theme.css".source =
     ./midnight-tokyo-night-renogare.theme.css;
-  home.file.".config/vesktop/settings/quickCss.css" = ./quickCss.css;
-  home.file.".config/vesktop/settings/settings.json" = ./settings.json;
+  home.file.".config/vesktop/settings/quickCss.css".source = ./quickCss.css;
+  home.file.".config/vesktop/settings/settings.json".source = ./settings.json;
 }
