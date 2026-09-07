598e35b703ec528be2bd7841340a95577cb4b967
aonkeeper4 <aonkeeper4@gmail.com>
2026-09-07 12:44:52 +0100

    update vesktop theme

--- home/vesktop/default.nix
+++ home/vesktop/default.nix
@@ -3,8 +3,11 @@
   home.packages = with pkgs; [
     vesktop
   ];
+
+  # config files
+  home.file.".config/vesktop/settings.json".source = ./vesktop-settings.json;
   home.file.".config/vesktop/themes/midnight-tokyo-night-renogare.theme.css".source =
     ./midnight-tokyo-night-renogare.theme.css;
   home.file.".config/vesktop/settings/quickCss.css".source = ./quickCss.css;
-  home.file.".config/vesktop/settings/settings.json".source = ./settings.json;
+  home.file.".config/vesktop/settings/settings.json".source = ./plugin-settings.json;
 }
